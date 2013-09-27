7ed6b3981ca19710765f7504a4cb79bb69ab45be
Tom Bannink <tombannink@gmail.com>
2013-09-27 22:24:10 +0200

    Longest common subsequence (very similar to levenshtein)

---
+++ src/longest-common-subsequence.cpp
@@ -0,0 +1,22 @@
+
+//Longest common subsequence.
+//IS NOT longest common subSTRING
+//substring is consecutive characters, subsequence is not
+
+int table[1024][1024];
+
+int LCSLength(const string& word1, const string& word2)
+{
+    for(int i = 0; i <= (int)word1.size(); ++i) table[i,0] = 0;
+    for(int j = 0; j <= (int)word2.size(); ++j) table[0,j] = 0;
+
+    for(int i = 1; i < (int)word1.size(); ++i)
+    {
+        for(int j = 1; j < (int)word2.size(); ++j)
+        {
+            if( word1[i-1] == word2[j-1] ) table[i][j] = table[i-1][j-1] + 1;
+            else table[i][j] = max( table[i-1][j] , table[i][j-1] );
+        }
+    }
+    return table[word1.size()][word2.size()];
+}
